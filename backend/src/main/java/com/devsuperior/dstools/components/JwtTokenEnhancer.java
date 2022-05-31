528c4b8c07b70440f9b48d103657b0d06a06186d
westorres9 <wester.torres9@gmail.com>
2022-05-31 09:21:25 -0300

    implementado componente JwtTokenEnhancer

---
+++ backend/src/main/java/com/devsuperior/dstools/components/JwtTokenEnhancer.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dstools.components;
+
+import com.devsuperior.dstools.entities.User;
+import com.devsuperior.dstools.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtTokenEnhancer implements TokenEnhancer {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+
+        User user = userRepository.finByEmail(authentication.getName());
+        Map<String, Object> map = new HashMap<>();
+        map.put("userFirstName", user.getFirstName());
+        map.put("userId", user.getId());
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
+        token.setAdditionalInformation(map);
+
+        return accessToken;
+    }
+
+}
