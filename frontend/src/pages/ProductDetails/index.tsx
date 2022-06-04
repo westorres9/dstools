958473c20dc346eb4394dd896092658f9d1b2e3a
westorres9 <wester.torres9@gmail.com>
2022-06-03 21:56:08 -0300

    estrutura html da pagina ProductDetails

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,6 +1,27 @@
 const ProductDetails = () => {
     return (
-        <h1>Pagina Detalhes do Produto</h1>
+        <div className="product-details-container">
+            <div className="product-details-card">
+                <div className="goback-container">
+
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+
+                        </div>
+                        <div className="name-price-container">
+
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
